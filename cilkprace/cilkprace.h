0323148fd653dccc55dc38736e1a491b2cd7e8e0
Daniel DeLayo <ddelayo@cs.stonybrook.edu>
2026-02-25 11:08:49 -0500

    Outputting pedigree

--- cilkprace/cilkprace.h
+++ cilkprace/cilkprace.h
@@ -155,7 +155,7 @@ public:
 
   void register_write(uint64_t addr, size_t num_bytes, source_loc_t store) {
     outs_red << "WRITE with pedigree " << __cilkrts_get_pedigree().rank << std::endl;
-    outs_red << "WRITE with pedigree " << "[REDACTED]" << std::endl;
+    //outs_red << "WRITE with pedigree " << "[REDACTED]" << std::endl;
   //  stack.register_write(addr, num_bytes, store);
   }
   void register_read(uint64_t addr, size_t num_bytes, source_loc_t store) {
--- cilkprace/race_detection_examples/blah.cpp
+++ cilkprace/race_detection_examples/blah.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+//#include <iostream>
 //#include <cilk/cilk.h>
 
 void blah(int& n) {
