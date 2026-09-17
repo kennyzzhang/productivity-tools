28a5f3c34d3ce72f27b852e3042552dc84e495f3
Daniel DeLayo <dandelayo@gmail.com>
2026-09-17 15:45:49 -0400

    fix build scripts

--- cilkprace/race_detection_examples/tools/build.sh
+++ cilkprace/race_detection_examples/tools/build.sh
@@ -58,8 +58,8 @@ cmake                                                                          \
   -DCMAKE_BUILD_TYPE="RelWithDebInfo"                                          \
   -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O3 -g "    \
   -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O3 -g"   \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS="ON"                                         \
-  -DDISABLED_TESTS="fibred;heat;fft"                                           \
+  -DENABLE_STATIC_RACE_DETECTION=ON                                            \
+  -DDISABLED_TESTS="fibred;heat;fft;blah;conditional_sync;funny_malloc;heap_reuse;label_test;loop;memcpy_race;missed_race;nqueens_racy;single_spawn_norace;single_spawn_race;single_spawn_rr_norace;single_spawn_rw_race;stack_reuse;strdup" \
   "$SOURCE_DIR"                                                                \
 ;
 
