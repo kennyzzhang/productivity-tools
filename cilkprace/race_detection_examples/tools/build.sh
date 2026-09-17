e97d1978b650d8b500f059083afe4c61efd8c474
Daniel DeLayo <dandelayo@gmail.com>
2026-09-17 15:42:05 -0400

    static race detection

--- cilkprace/race_detection_examples/tools/build.sh
+++ cilkprace/race_detection_examples/tools/build.sh
@@ -58,8 +58,9 @@ cmake                                                                          \
   -DCMAKE_BUILD_TYPE="RelWithDebInfo"                                          \
   -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O3 -g "                                     \
   -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O3 -g"                                    \
-  -DDISABLED_TESTS="fibred;heat;fft;blah;conditional_sync;funny_malloc;heap_reuse;label_test;loop;memcpy_race;missed_race;nqueens_racy;single_spawn_norace;single_spawn_race;single_spawn_rr_norace;single_spawn_rw_race;stack_reuse;strdup" \
   -DDISABLED_TESTS="fibred;heat;fft;blah"                                      \
+  -DDISABLED_TESTS="fibred;heat;fft;blah;conditional_sync;funny_malloc;heap_reuse;label_test;loop;memcpy_race;missed_race;nqueens_racy;single_spawn_norace;single_spawn_race;single_spawn_rr_norace;single_spawn_rw_race;stack_reuse;strdup" \
+  -DENABLE_STATIC_RACE_DETECTION=ON                                            \  
   "$SOURCE_DIR"                                                                \
 ;
 
