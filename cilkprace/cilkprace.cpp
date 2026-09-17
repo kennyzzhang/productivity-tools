f67b751cc6d97c73199c4b4567712c8dd346599e
Daniel DeLayo <dandelayo@gmail.com>
2026-09-17 14:06:47 -0400

    move to cpp

--- cilkprace/cilkprace.cpp
+++ cilkprace/cilkprace.cpp
@@ -32,13 +32,50 @@ void CilkpraceImpl_t::report_read_race(uintptr_t addr, csi_id_t load_id,
   _exit(EXIT_FAILURE);
 }
 
+__attribute__((visibility("default")))
+bool shadow_label::does_read_race(const os_label &reader) {
+    unsigned lca_depth = active_reader.lca(reader);
+    if (lca_depth > write_depth) {
+        write_depth = lca_depth;
+    }
+    if (write_depth % 4 == 3) {
+        return true;
+    }
+    if (lca_depth % 4 != 3) {
+        active_reader = reader;
+    } else {
+        // Technically unnecessary for one-worker execution, but could help
+        // prune later lca calls
+        active_reader.end_idx = lca_depth;
+    }
+    return false;
+}
+
+__attribute__((visibility("default")))
+bool shadow_label::does_write_race(const os_label &writer) {
+    unsigned lca_depth = active_reader.lca(writer);
+    if (lca_depth > write_depth) {
+        write_depth = lca_depth;
+    }
+    if (write_depth % 4 == 3) {
+        return true;
+    }
+    if (lca_depth % 4 != 3) {
+        active_reader = writer;
+        write_depth = writer.end_idx;
+    } else {
+        return true;
+    }
+    return false;
+}
+
 // Force external symbols for inline member functions so they are always exported by the runtime dylib
 __attribute__((visibility("default")))
 void __cilkprace_export_anchor() {
-//  volatile auto p1 = &os_label::range_relation;
-//  volatile auto p2 = &os_label::is_identical_slow;
-//  (void)p1;
-//  (void)p2;
+  volatile auto p1 = &shadow_label::does_read_race;
+  volatile auto p2 = &shadow_label::does_write_race;
+  (void)p1;
+  (void)p2;
 }
 
 
